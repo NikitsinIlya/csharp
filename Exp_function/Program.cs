7848184253b8720f5a701ab9b3a781b705c0844a
NikitsinIlya <JamsN87@gmail.com>
2023-01-22 11:07:30 +0300

    add fin Function

--- Exp_function/Program.cs
+++ Exp_function/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// запись функции f(x)= x²+1
+/*
+double f(double x)
+{
+    double result = x * x + 1;
+    return result
+}
+*/
+
+// функция нахождения максимума
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+/*
+int a1 = 4;
+int b1 = 44;
+int c1 = 454;
+int a2 = 44;
+int b2 = 41;
+int c2 = 94;
+int a3 = 34;
+int b3 = 454;
+int c3 = 84;
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+*/ 
+// Заменим ввод переменных массивом
+
+// индексы       0   1   2   3   4   5  6   7   8
+int[] array = { 31, 82, 13, 46, 55, 65, 74, 8, 94 };
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+
+Console.WriteLine(max);
