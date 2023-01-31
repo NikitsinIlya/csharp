e2692b77237b4e86ec5be505720d0467b77eef08
IlyaNikitsin <JamsN87@gmail.com>
2023-01-31 09:20:19 +0300

    add for_in_for

--- Ex_FOR/Program.cs
+++ Ex_FOR/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
