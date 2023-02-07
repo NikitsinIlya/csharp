114351bd7f9648d32b023d0fa542d92f0da5b2df
IlyaNikitsin <JamsN87@gmail.com>
2023-02-07 13:58:03 +0300

    add rekursiya

--- Ex_Rekursiya/Program.cs
+++ Ex_Rekursiya/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+/*
+double Factorial (int n)  // функция нахождения факториала рекурсией
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);       //// Рекурсия это вызов функции в функции!
+}
+Console.WriteLine("Введите факториал числа: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(n));
+*/
+
+// выведем последовательность фебоначчи рекурсией
+
+int Febonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Febonacci(n-1) + Febonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)                                       // если искать больше 40 чисел рекурсия очень сильно перегружает систему! Лучше использовать цикл
+{
+    Console.WriteLine(Febonacci(i));
+}
